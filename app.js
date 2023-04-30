c84a6857dbbe43e950d295962976a24fcb5eb02c
sujith varghese <sujithvargheseofficial250@gmail.com>
2023-05-01 01:53:38 +0530

    admin table and form complete

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+ var db =require('./connection/connection')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
+
+var fileUpload=require('express-fileupload')
+
+
+var hbs = require('express-handlebars');
 
 var app = express();
 
@@ -13,14 +19,30 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+
+// view engine setup
+db.connect((err)=>{
+  if(err) console.log('connection error'+err);
+  else console.log('database connected');
+})
+
+app.use(fileUpload())
+app.engine('hbs', hbs.engine({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layout',
+  partialsDir: __dirname + '/views/partials'
+}))
+ 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('admin/view-products',{admin:true});
+});
+router.get('/add-product',(req,res)=>{
+  res.render('admin/add-product')
+})
+router.post('/add-product',(req,res)=>{
+  console.log(req.body)
+  console.log(req.files.image)
+})
+module.exports = router;
